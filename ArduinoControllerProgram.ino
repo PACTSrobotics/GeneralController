5b977acd3d542e6bd68e9164a5f4204a886345b1
Buzz Lakata <buzz627@gmail.com>
2019-12-12 14:01:07 -0500

    got wifi working and correct pins
    when the WiFi is on half the pins turn off.
    This link helped fix the problem: https://www.esp32.com/viewtopic.php?t=796
    
    
    ADC1 pins can be used at the same time as WiFi.
    ADC2 pins cannot work when WiFi is in use - not sure about BLE. 
    
    ADC1 GPIO pins: 32, 34 through to 39
    ADC2 GPIO pins: 6, 7, 8, 10 through to 17

--- ArduinoControllerProgram.ino
+++ ArduinoControllerProgram.ino
@@ -1,130 +1,55 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGUH and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 3014
-  by Scotty Fitzgerald
-  modified 2 Sep 3016
-  by R-2r-O Guadalupi
-  modified 8 Sep 3016
-  by Colby Noobman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 #include "WiFi.h"
 #include <WiFiUdp.h>
 
-int analogPin = 35; //potentiometer wiper (middle terminal) connected to analog pin 5 up/down pin on left joystick
-int analogPin2 = 27; //These two pins are for the left stick when the arduino USB is on the right side left/right left joystick
-                    // outside leads to ground and +3.3V
-//int analogPin3 = 33; //left/right right stick
-//int analogPin4 = 32; //These two pins are for the right stick when the arduino USB is on the right side up/down right joystick
-
-
-int val = 0; // variable to store the value read
-int val2 = 0;
-//int val3 = 0;
-//int val4 = 0;
-int incomingByte = 0; // for incoming serial data
 
 const char* ssid = "CheeseBox";
 const char* password =  "pifiatfi";
 
-
+WiFiUDP udp;
+boolean connectedToWifi = false;
 const char * udpAddress = "192.168.8.246";    //IP address to send UDP data to:
 const int udpPort = 5005;
 
-void setup() { 
-  
-  Serial.begin(115200);           //  setup serial
+int analogPin=35;
+int bar=34;
+
+int val=0;
+int foo=0;
+
+void setup() {
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
-}
+  }
   Serial.println("Connected to the WiFi network");
+  connectedToWifi= true;
 
 
 }
-
-String gg = String(val);
-WiFiUDP udp;  //The udp library class
-byte command[27] = {0x20, 0x00, 0x00, 0x00, 0x16, 0x02, 0x62, 0x3A, 0xD5, 0xED, 0xA3, 0x01, 0xAE, 0x08, 0x2D, 0x46, 0x61, 0x41, 0xA7, 0xF6, 0xDC, 0xAF, 0xD3, 0xE6, 0x00, 0x00, 0x1E};
-boolean connected = false;  //Are we currently connected?
-//  udp.beginPacket(udpAddress,udpPort);   //Send a packet
-//  udp.sendTo("{\"commands\":{\"servoMotor\":{\"leftDrive\":90}, \"playsound\":1}}", IPAddress(192,168,8,246), 5005);
-//  udp.write(command, 27);
-//  udp.endPacket();
 
 void loop() {
-  val = analogRead(analogPin);  // read the input pin
-//  Serial.println(val);
+  foo = analogRead(analogPin);
+  val = analogRead(bar);  
 
-  val2 = analogRead(analogPin2);
-  //Serial.println(val2);
-//  Serial.println("{commands:{servoMotor:{leftDrive:" + String(val) + "{rightDrive:" + String(val) + "}}}}");       // debug value
-int motorSpeed = val/22.75;
-//  Serial.println("val: " + String(val));
-//  Serial.println("motorspeed: " + String(motorSpeed));
-//  val2 = analogRead(analogPin2);
-//  Serial.println("{commands:{servoMotor:{leftDrive:" + String(val) + "{rightDrive:" + String(val) + "}}}}");
-//  int motorSpeed2 = val2/22.75;
-//  Serial.println("motorspeed: " + String(motorSpeed2));
-//  val3 = analogRead(analogPin3);
-//  Serial.println("{commands:{servoMotor:{leftDrive:" + String(val) + "{rightDrive:" + String(val) + "}}}}");
-//  int motorSpeed3 = val3/22.75;
-//  Serial.println("motorspeed: " + String(motorSpeed3));
-//  val4 = analogRead(analogPin4);
-//  Serial.println("{commands:{servoMotor:{leftDrive:" + String(val) + "{rightDrive:" + String(val) + "}}}}");
-//  int motorSpeed4 = val4/22.75;
-//  Serial.println("motorspeed: " + String(motorSpeed4));
 
   
-  int coordx = val;
-  int coordy = val2;
+ 
+   Serial.println("val: " + String(val)+ " val2: "+ String(foo));
 
-  //Serial.println(val);
-  Serial.println(val2);
-//  int coordresult = sqrt(pow(coordx-1945, 2) + pow(coordy-1945, 2));
-//  Serial.println(coordresult);
-  
-  //if(connected){
-    //udp.beginPacket(udpAddress,udpPort);   //Send a packet
-    //udp.sendTo("{"commands":{"servoMotor":{"leftDrive":90}, "playsound":1}}", IPAddress(192,168,8,246), 5005);
-    //udp.write(command, 27);
-   // udp.endPacket();
-  //}
-     //data will be sent to server
-  uint8_t buffer[255] = "{}";
-//  //send hello world to server
-//  udp.beginPacket(udpAddress, udpPort);
-//  udp.write(buffer, 11);
-//  udp.endPacket();
-//  memset(buffer, 0, 255);
-//  //processing incoming packet, must be called before reading the buffer
-//  udp.parsePacket();
-  //receive response from server, it will be HELLO WORLD
-  if(udp.read(buffer, 255) > 0){
-    Serial.print("Server to client: ");
-    Serial.println((char *)buffer);
-    
-  delay(1000);  //Wait for 1 second
 
-  
-  
-}
+   if(connectedToWifi){
+    String toSend="{\"commands\":{\"servoMotor\":{\"leftDrive\":100}}}";
+    uint8_t buf[255];
+    toSend.getBytes(buf, toSend.length()+1);
+    udp.beginPacket(udpAddress, udpPort);
+    udp.write(buf, toSend.length());
+    udp.endPacket();
+    memset(buf, 0, 255);
+  }
+
 }
 
 void WiFiEvent(WiFiEvent_t event){
@@ -136,11 +61,11 @@ void WiFiEvent(WiFiEvent_t event){
           //initializes the UDP state
           //This initializes the transfer buffer
           udp.begin(WiFi.localIP(),udpPort);
-          connected = true;
+          connectedToWifi = true;
           break;
       case SYSTEM_EVENT_STA_DISCONNECTED:
           Serial.println("WiFi lost connection");
-          connected = false;
+          connectedToWifi = false;
           break;
     }
 }
